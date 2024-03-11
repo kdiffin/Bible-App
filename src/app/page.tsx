a40a60d6a697002076233dac3e2db089a361836b
Rohantayron <bobonica111@gmail.com>
2024-03-11 16:24:04 +0200

    committry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Bible App</div>;
+  return (
+    <div>
+      <div className="flex gap-x-7">
+        <div className="font-bold mr-auto">BibleApp</div>
+        <Button variant="outline">read</Button>
+        <div>about</div>
+        <div>contact</div>
+        <Button variant="outline" className="ml-auto">
+          read
+        </Button>
+      </div>
+    </div>
+  );
 }
