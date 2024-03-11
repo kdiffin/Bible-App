f522777798050e864dc47a1a48e4ad2247e0300d
kdiffin <vhiunese@gmail.com>
2024-03-11 20:09:14 +0400

    new stuff added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col ">
+    <main className="flex a min-h-screen flex-col ">
       <Navbar />
 
       <Hero />
